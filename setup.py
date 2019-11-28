38eeed8fbdffa3a8f269cbdeb43f87261b85f93c
AdrienPercevault <adrien.percevault@gmail.com>
2019-11-28 17:08:29 +0100

    [Refacto] Setup : add tensorflow

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='gym_Rubiks_Cube',
-      version='0.0.1',
-      install_requires=['gym', 'termcolor'],
-      url="https://github.com/RobinChiu/gym-Rubiks-Cube",
-      packages=find_packages()
+setup(
+    name='gym_Rubiks_Cube',
+    version='0.0.1',
+    install_requires=[
+        'gym', 
+        'termcolor',
+        'tensorflow'
+    ],
+    url="https://github.com/RobinChiu/gym-Rubiks-Cube",
+    packages=find_packages()
 )  
